9146920410b58f8c5adaa1bcfb50541ad329458b
adrian aguilar <adrianaguilar@macbook-air-de-adrian.local>
2018-05-19 18:51:45 -0500

    Initial Commit

---
+++ com.altura/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  com.altura
+//
+//  Created by adrian aguilar on 19/5/18.
+//  Copyright © 2018 altura s.a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
